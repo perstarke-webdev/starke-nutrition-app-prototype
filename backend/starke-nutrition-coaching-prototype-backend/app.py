ec06cb2eaff8f4915e6554e533f6ab58bec8ee59
Per Starke <per@starke-team.de>
2024-02-21 09:41:50 +1100

    Change Python function for getting recipe so that nutrients are given as parameters, not hard-coded

--- backend/starke-nutrition-coaching-prototype-backend/app.py
+++ backend/starke-nutrition-coaching-prototype-backend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, session
+from flask import Flask, session, request
 from dotenv import load_dotenv
 import requests
 import datetime
 import os
+
+from werkzeug.exceptions import BadRequestKeyError
 
 app = Flask(__name__)
 
@@ -32,7 +34,7 @@ def write_recipes_in_list(kcal, proteins, carbs, fats):
     Creates a list of recipes matching the give nutrients within a 10% range
     """
 
-    session["recipe_counter"] = 0
+    session["recipe_counters"] = 0
 
     allowed_range = 0.1
 
@@ -54,27 +56,85 @@ def write_recipes_in_list(kcal, proteins, carbs, fats):
                    "minCarbs": min_carbs, "maxCarbs": max_carbs,
                    "minFat": min_fats, "maxFat": max_fats}
 
-    return requests.request("GET", url + "recipes/findByNutrients", headers=headers, params=querystring).json()
+    response = requests.request("GET", url + "recipes/findByNutrients", headers=headers, params=querystring)
+
+    if response.status_code != 200:
+        print("Request failed with status code:", response.status_code)
+        print(response.text)
+        return "Error"
+    else:
+        try:
+            json_response = response.json()
+            print(json_response)
+        except ValueError as e:
+            print("Failed to parse JSON response:", e)
+            return "Error"
+
+    return json_response
 
 
 @app.route("/get_recipe")
 def get_recipe():
     """
-    Get a recipe with specified nutrients
+    Get a random recipe with specified nutrients.
+    Call with parameters to create new recipe list,
+    call without to get a new recipe with same nutrients as from the call before
     """
 
+    # Get nutrients from URL
+    try:
+        session["kcal"] = request.args["kcal"]
+    except BadRequestKeyError:
+        if "kcal" in session.keys():
+            pass
+        else:
+            session["kcal"] = 0
+    try:
+        session["proteins"] = request.args["proteins"]
+    except BadRequestKeyError:
+        if "proteins" in session.keys():
+            pass
+        else:
+            session["proteins"] = 0
+    try:
+        session["carbs"] = request.args["carbs"]
+    except BadRequestKeyError:
+        if "carbs" in session.keys():
+            pass
+        else:
+            session["carbs"] = 0
+    try:
+        session["fats"] = request.args["fats"]
+    except BadRequestKeyError:
+        if "fats" in session.keys():
+            pass
+        else:
+            session["fats"] = 0
+
+    # Route has been called before
     if "recipes_counter" in session.keys():
+
+        # Nutrients given, create new recipe list
+        if session["kcal"] and session["proteins"] and session["carbs"] and session["fats"]:
+            session["recipes"] = write_recipes_in_list(session["kcal"], session["proteins"], session["carbs"], session["fats"])
+            session["recipes_counter"] = 0
+            return session["recipes"][0]
+
         session["recipes_counter"] += 1
 
+        # List of recipes has been fully iterated
         if session["recipes_counter"] == 9:
             session["recipes_counter"] = 0
-            session["recipes"] = write_recipes_in_list(500, 50, 50, 10)
+            session["recipes"] = write_recipes_in_list(session["kcal"], session["proteins"], session["carbs"], session["fats"])
 
         return session["recipes"][session["recipes_counter"]]
 
+    # First ever call
     else:
-        session["recipes"] = write_recipes_in_list(500, 50, 50, 10)
+        session["recipes"] = write_recipes_in_list(session["kcal"], session["proteins"], session["carbs"], session["fats"])
         session["recipes_counter"] = 0
         return session["recipes"][0]
 
 
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
