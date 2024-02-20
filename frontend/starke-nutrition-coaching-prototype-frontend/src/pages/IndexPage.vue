42b8f5003a341b0671eeb53b1df9b9a786d4c9b2
Per Starke <per@starke-team.de>
2024-02-20 13:14:43 +1100

    improve recipe recommendation placeholder

--- frontend/starke-nutrition-coaching-prototype-frontend/src/pages/IndexPage.vue
+++ frontend/starke-nutrition-coaching-prototype-frontend/src/pages/IndexPage.vue
@@ -25,15 +25,41 @@
         </tbody>
       </q-markup-table>
 
+
       <q-btn color="primary" class="q-mt-md" label="Submit" />
 
-      <div class="text-h5 q-pt-lg">Suggested recipe</div>
-
-      <q-img src="~assets/oatbar.webp" width="80%" style="max-width: 600px;" class="q-mt-md"/>
-
     </div>
+
+      <div class="text-h5 q-mb-md">Suggested recipe</div>
+
+      <div class="image-container">
+        <q-img src="~assets/oatbar.webp" class="recipe-img" />
+        <div class="image-overlay"></div>
+        <div class="text-h5 text-bold recipe-title">Oat-Nut-Bar</div>
+      </div>
+
+      <div class="q-pt-sm">
+      <q-text>
+        {{kcal}} kcal,
+        {{proteins}}g proteins,
+        {{carbs}}g carbs,
+        {{fats}}g fat
+      </q-text>
+      </div>
+
+    <q-btn color="primary" class="q-mt-md" label="Recipe details" />
+    <br>
+
+    <div class="q-gutter-x-md q-mb-xl">
+      <q-btn color="primary" class="q-mt-md" label="Generate new recipe" />
+      <q-btn color="primary" class="q-mt-md" label="Send recipe to trainee" />
+    </div>
+
   </q-page>
 </template>
+
+
+
 
 <script>
 import { defineComponent } from 'vue';
@@ -50,3 +76,40 @@ export default defineComponent({
   },
 });
 </script>
+
+
+
+
+<style lang="scss">
+
+.image-container {
+  position: relative;
+}
+
+.image-overlay {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  max-width: 600px;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+}
+
+.recipe-img {
+  width: 80%;
+  max-width: 600px;
+}
+
+.recipe-title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  z-index: 2;
+}
+
+</style>
