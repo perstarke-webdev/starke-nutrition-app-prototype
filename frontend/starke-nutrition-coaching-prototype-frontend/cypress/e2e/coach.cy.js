1a71f50cebc68ba3c35e8a19eede971750d390b6
Per Starke <per@starke-team.de>
2024-02-24 11:18:37 +1100

    Test inputting and displaying numbers in nutrient table

--- frontend/starke-nutrition-coaching-prototype-frontend/cypress/e2e/coach.cy.js
+++ frontend/starke-nutrition-coaching-prototype-frontend/cypress/e2e/coach.cy.js
@@ -10,7 +10,8 @@ describe('Coach page', () => {
 
   })
 
-  it.only('Nutritional table displays 0 for all values at first', () => {
+
+  it('Nutritional table displays 0 for all values at first', () => {
 
     cy.visit('http://localhost:9000/#/coach')
 
@@ -20,4 +21,52 @@ describe('Coach page', () => {
     cy.get("[data-test='input']").eq(3).invoke('val').should('eq', '0');
 
   })
+
+
+   it.only('Nutritional table displays inputted values', () => {
+
+      cy.visit('http://localhost:9000/#/coach')
+
+      // Input a number into the q-input element
+      const inputNumber = 500; // The number you want to test
+      cy.get("[data-test='input']").eq(0).type(inputNumber);
+
+      // Verify that the inputted number is displayed
+      cy.get("[data-test='input']").eq(0)
+        .invoke('val')
+        .should('eq', inputNumber.toString());
+
+
+      // Input a number into the q-input element
+      const inputNumber2 = 50; // The number you want to test
+      cy.get("[data-test='input']").eq(1).type(inputNumber2);
+
+      // Verify that the inputted number is displayed
+      cy.get("[data-test='input']").eq(1)
+        .invoke('val')
+        .should('eq', inputNumber2.toString());
+
+
+      // Input a number into the q-input element
+      cy.get("[data-test='input']").eq(2).type(inputNumber2);
+
+      // Verify that the inputted number is displayed
+      cy.get("[data-test='input']").eq(2)
+        .invoke('val')
+        .should('eq', inputNumber2.toString());
+
+
+
+      // Input a number into the q-input element
+      const inputNumber3 = 10; // The number you want to test
+      cy.get("[data-test='input']").eq(3).type(inputNumber3);
+
+      // Verify that the inputted number is displayed
+      cy.get("[data-test='input']").eq(3)
+        .invoke('val')
+        .should('eq', inputNumber3.toString());
+
+     })
+
+
 })
