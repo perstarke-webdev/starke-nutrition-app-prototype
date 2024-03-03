2b08dc0f5c12e9ce53046e6c9ce3d75e39e19067
Per Starke <per@starke-team.de>
2024-03-03 12:28:04 +1100

    show "successfully sent to trainee"

--- frontend/starke-nutrition-coaching-prototype-frontend/src/pages/CoachView.vue
+++ frontend/starke-nutrition-coaching-prototype-frontend/src/pages/CoachView.vue
@@ -1,6 +1,5 @@
 <template>
   <q-page class="text-center">
-
     <div class="text-h5 q-pt-lg">Nutritional requirements</div>
 
     <div class="q-pa-md">
@@ -8,66 +7,97 @@
         <tbody>
         <tr>
           <td class="text-center text-bold">Kcal</td>
-
-            <q-input input-class="text-center"
-                     data-test="input"
-                     outlined
-                     :select-on-focus="true"
-                     v-model="kcal_wanted"
-                     type="number" />
-
+          <q-input
+            input-class="text-center"
+            data-test="input"
+            outlined
+            :select-on-focus="true"
+            v-model="kcal_wanted"
+            type="number"
+          />
         </tr>
         <tr>
           <td class="text-center text-bold">Proteins</td>
-            <q-input input-class="text-center" data-test="input" outlined v-model="proteins_wanted" type="number" />
+          <q-input
+            input-class="text-center"
+            data-test="input"
+            outlined
+            v-model="proteins_wanted"
+            type="number"
+          />
         </tr>
         <tr>
           <td class="text-center text-bold">Carbs</td>
-            <q-input input-class="text-center" data-test="input" outlined v-model="carbs_wanted" type="number" />
+          <q-input
+            input-class="text-center"
+            data-test="input"
+            outlined
+            v-model="carbs_wanted"
+            type="number"
+          />
         </tr>
         <tr>
           <td class="text-center text-bold">Fats</td>
-            <q-input input-class="text-center" data-test="input" outlined v-model="fats_wanted" type="number" />
+          <q-input
+            input-class="text-center"
+            data-test="input"
+            outlined
+            v-model="fats_wanted"
+            type="number"
+          />
         </tr>
         </tbody>
       </q-markup-table>
-
-
-      <q-btn data-test="submit-btn" color="primary" class="q-mt-md" label="Submit" @click="submitForm" />
-
-
+      <q-btn
+        data-test="submit-btn"
+        color="primary"
+        class="q-mt-md"
+        label="Submit"
+        @click="submitForm"
+      />
     </div>
 
-      <div class="text-h5 q-mb-md">Suggested recipe</div>
+    <div class="text-h5 q-mb-md">Suggested recipe</div>
 
-      <div class="image-container">
-        <q-img data-test="recipe-img" :src="recipe_img_path" class="recipe-img" />
-        <div class="image-overlay"></div>
-        <div class="text-h5 text-bold recipe-title">{{recipe_title}}</div>
-      </div>
+    <div class="image-container">
+      <q-img data-test="recipe-img" :src="recipe_img_path" class="recipe-img" />
+      <div class="image-overlay"></div>
+      <div class="text-h5 text-bold recipe-title">{{recipe_title}}</div>
+    </div>
 
-      <div class="q-pt-sm">
+    <div class="q-pt-sm">
       <q-text data-test="received-nutrients">
-        {{recipe_kcal}} kcal,
-        {{recipe_proteins}} proteins,
-        {{recipe_carbs}} carbs,
-        {{recipe_fats}} fat
+        {{recipe_kcal}} kcal, {{recipe_proteins}} proteins, {{recipe_carbs}} carbs, {{recipe_fats}} fat
       </q-text>
-      </div>
+    </div>
 
-    <q-btn color="primary" class="q-mt-md" label="Recipe details"/>
-    <br>
+    <q-btn color="primary" class="q-mt-md" label="Recipe details" />
+    <br />
 
     <div class="q-gutter-x-md q-mb-xl">
-      <q-btn color="primary" class="q-mt-md" label="Generate new" @click="submitForm"/>
-      <q-btn color="primary" class="q-mt-md" label="Send to trainee" @click="sendToTrainee" />
+      <q-btn
+        color="primary"
+        class="q-mt-md"
+        label="Generate new"
+        @click="submitForm"
+      />
+      <q-btn
+        v-if="!sendToTraineeSuccess"
+        :color="sendToTraineeSuccess ? 'secondary' : 'primary'"
+        :class="{ 'q-opacity-half': sendToTraineeSuccess }"
+        class="q-mt-md"
+        label="Send to trainee"
+        @click="handleSendToTrainee"
+      />
+      <q-btn
+        v-if="sendToTraineeSuccess"
+        :color="sendToTraineeSuccess ? 'secondary' : 'primary'"
+        class="q-mt-md"
+        label="Successfully sent to trainee"
+      />
     </div>
-
   </q-page>
 </template>
-
-
-
 
 <script>
 import { defineComponent } from 'vue';
@@ -89,53 +119,60 @@ export default defineComponent({
       recipe_carbs: '0g',
       recipe_fats: '0g',
       recipe_img_path: null,
+      sendToTraineeSuccess: false,
     };
   },
 
-  methods: {
-
-    submitForm() {
-      axios.get('http://127.0.0.1:8000/get_recipe', {
-        params: {
-          kcal: this.kcal_wanted,
-          proteins: this.proteins_wanted,
-          carbs: this.carbs_wanted,
-          fats: this.fats_wanted,
-        }
-      })
-      .then(response => {
-        this.recipe_id = response.data.id;
-        this.recipe_title = response.data.title;
-        this.recipe_kcal = response.data.calories;
-        this.recipe_proteins = response.data.protein;
-        this.recipe_carbs = response.data.carbs;
-        this.recipe_fats = response.data.fat;
-        this.recipe_img_path = response.data.image;
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+  computed: {
+    sendToTraineeButtonLabel() {
+      return this.sendToTraineeSuccess ? 'Successfully sent to trainee' : 'Send to trainee';
     },
+    sendToTraineeButtonColor() {
+      return this.sendToTraineeSuccess ? 'secondary' : 'primary';
+    },
+  },
 
-
-    deleteAllRecipes() {
-      axios.get('http://127.0.0.1:8000/delete_all_recipes')
-        .then(response => {
-          console.log('All recipes deleted successfully:', response);
+  methods: {
+    submitForm() {
+      axios
+        .get('http://127.0.0.1:8000/get_recipe', {
+          params: {
+            kcal: this.kcal_wanted,
+            proteins: this.proteins_wanted,
+            carbs: this.carbs_wanted,
+            fats: this.fats_wanted,
+          },
         })
-        .catch(error => {
+        .then((response) => {
+          this.recipe_id = response.data.id;
+          this.recipe_title = response.data.title;
+          this.recipe_kcal = response.data.calories;
+          this.recipe_proteins = response.data.protein;
+          this.recipe_carbs = response.data.carbs;
+          this.recipe_fats = response.data.fat;
+          this.recipe_img_path = response.data.image;
+        })
+        .catch((error) => {
           console.error('Error:', error);
         });
     },
 
-
-
+    deleteAllRecipes() {
+      axios
+        .get('http://127.0.0.1:8000/delete_all_recipes')
+        .then((response) => {
+          console.log('All recipes deleted successfully:', response);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    },
 
     sendToTrainee() {
-
       this.deleteAllRecipes();
 
-      axios.get('http://127.0.0.1:8000/write_recipe', {
+      axios
+        .get('http://127.0.0.1:8000/write_recipe', {
           params: {
             id: this.recipe_id,
             title: this.recipe_title,
@@ -148,30 +185,31 @@ export default defineComponent({
             wanted_proteins: this.proteins_wanted,
             wanted_carbs: this.carbs_wanted,
             wanted_fats: this.fats_wanted,
-          }
+          },
         })
-        .then(response => {
+        .then((response) => {
           console.log('Response:', response);
+          this.sendToTraineeSuccess = true;
+          setTimeout(() => {
+            this.sendToTraineeSuccess = false;
+          }, 3000);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error:', error);
         });
+    },
 
-  }
-
+    handleSendToTrainee() {
+      this.sendToTrainee();
+      setTimeout(() => {
+        this.sendToTraineeSuccess = false;
+      }, 3000);
+    },
   },
-
-
-
 });
-
 </script>
 
-
-
-
 <style lang="scss">
-
 .image-container {
   position: relative;
 }
@@ -201,5 +239,4 @@ export default defineComponent({
   color: white;
   z-index: 2;
 }
-
 </style>
