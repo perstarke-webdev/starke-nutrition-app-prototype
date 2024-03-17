edc656ad207115c565e12a89b49e5ec487eb67e7
Per Starke <per@starke-team.de>
2024-03-17 13:17:06 +1100

    try using deployed DB with backend

--- backend/starke-nutrition-coaching-prototype-backend/app.py
+++ backend/starke-nutrition-coaching-prototype-backend/app.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float
 from flask import Flask, session, request, make_response, jsonify
 from flask_cors import CORS
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import requests
 import datetime
 import os
@@ -17,13 +17,11 @@ app.permanent_session_lifetime = datetime.timedelta(days=7)
 # Setup environmental variables and Spoonacular headers and URL
 parent_dir = os.path.dirname(os.getcwd())
 
-rapid_api_key = os.environ.get('RAPID_API_KEY')
+# rapid_api_key = os.environ.get('RAPID_API_KEY')
 
-"""
-Before, for local testing, it was:
+# Before, for local testing, it was:
 load_dotenv(parent_dir + "/Envs/key.env")
-os.getenv("RAPID_API_KEY")
-"""
+rapid_api_key = os.getenv("RAPID_API_KEY")
 
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
 headers = {
@@ -32,7 +30,8 @@ headers = {
 }
 
 # Create the database engine
-engine = create_engine("mysql+pymysql://root:24Zorg42@localhost/recipes")
+connection_string = "mysql+pymysql://root:12345@34.122.247.82/recipes"
+engine = create_engine(connection_string)
 
 # Define the metadata
 meta = MetaData()
@@ -260,7 +259,7 @@ def get_link():
 @app.route("/startpage")
 def startpage():
     return "<h1>Startpage</h1>"
-#
-#
+
+
 if __name__ == "__main__":
     app.run()
